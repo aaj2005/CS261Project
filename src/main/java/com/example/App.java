5013f8737a575f3d6672713d4900b16c6e8ac47b
Archii0 <archiedascombe@icloud.com>
2025-02-15 11:09:29 +0000

    added starter UI

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("proj"), 1920, 1200);
         stage.setScene(scene);
         stage.show();
     }
