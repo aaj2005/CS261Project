0666f0bcb9adc0d51302a716bc092b1a3019813b
Archii0 <archiedascombe@icloud.com>
2025-03-01 16:58:17 +0000

    added an alert for too many simulations

--- src/main/java/com/example/SimulationManager.java
+++ src/main/java/com/example/SimulationManager.java
@@ -1,5 +1,7 @@
 package com.example;
 import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class SimulationManager {
     private int numSimulations = 0;
@@ -10,6 +12,10 @@ public class SimulationManager {
     }
 
     public void addNewSimulation() {
+        if (simList.size() - 1 >= 8) {
+            showAlert("Simulation Limit Reached", "You can only have up to 8 simulations.");
+            return;
+        }
         int newSimPosition = simList.size() - 1;
         SimListItem newSimItem = new SimListItem("Simulation " + ++numSimulations);
         simList.add(newSimPosition, newSimItem);
@@ -18,4 +24,13 @@ public class SimulationManager {
     public void deleteSimulation(SimListItem item) {
         simList.remove(item);
     }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 }
