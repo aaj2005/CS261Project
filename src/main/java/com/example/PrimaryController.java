bcf3a7ef302da18429cea0d31fcdafa7a7d9c0a0
Archii0 <archiedascombe@icloud.com>
2025-03-01 16:58:37 +0000

    adding missing graph code from refactor

--- src/main/java/com/example/PrimaryController.java
+++ src/main/java/com/example/PrimaryController.java
@@ -14,14 +14,19 @@ public class PrimaryController {
     @FXML private Label lbl_duration, lbl_requests;
     @FXML private Button run_button;
 
+
+
     private SimulationManager simulationManager;
+    private ListCell<SimListItem> selectedCell;
 
     @FXML
     public void initialize() {
+//        simList.setPrefHeight(simList.getItems().size() * 50); // Adjust height based on items
         simulationManager = new SimulationManager(simList.getItems());
 
+
         simList.getItems().add(new SimListItem("New Simulation"));
-        simList.setCellFactory(param -> new SimulationCellFactory(simulationManager));
+        simList.setCellFactory(param -> new SimulationCellFactory(simulationManager, this));
         simList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         simList.getSelectionModel().clearSelection();
 
@@ -40,14 +45,43 @@ public class PrimaryController {
         crossing_requests.setDisable(!enabled);
     }
 
+
     private void loadSimulationData() {
-        VBox graph = Graph.createGraph(new SimulationData[] {
-                new SimulationData("Run 1", new double[]{2.5, 3.0}, new double[]{5.0, 6.0}, new double[]{3.2, 4.0})
-        });
+        SimulationData[] runs = new SimulationData[5];
+        runs[0] = new SimulationData("Run 1", new double[]{2.5, 3.0, 4.5, 1.5}, new double[]{5.0, 6.0, 8.0, 3.0}, new double[]{3.2, 4.0, 5.5, 2.5});
+        runs[1] = new SimulationData("Run 2", new double[]{2.8, 3.5, 4.8, 1.8}, new double[]{5.5, 6.5, 8.5, 3.5}, new double[]{3.5, 4.2, 5.8, 2.8});
+        runs[2] = new SimulationData("Run 3", new double[]{3.0, 3.8, 5.0, 2.0}, new double[]{6.0, 7.0, 9.0, 4.0}, new double[]{3.8, 4.5, 6.0, 3.0});
+        runs[3] = new SimulationData("Run 4", new double[]{2.6, 3.2, 4.6, 1.6}, new double[]{5.2, 6.2, 8.2, 3.2}, new double[]{3.3, 4.1, 5.6, 2.6});
+        runs[4] = new SimulationData("Run 5", new double[]{2.6, 3.2, 4.6, 1.6}, new double[]{5.2, 6.2, 8.2, 3.2}, new double[]{3.3, 4.1, 5.6, 2.6});
+
+
+
+        VBox graph = Graph.createGraph(runs);
+        graphContainer.setTopAnchor(graph, 0.0);
+        graphContainer.setBottomAnchor(graph, 0.0);
+        graphContainer.setLeftAnchor(graph, 0.0);
+        graphContainer.setRightAnchor(graph, 0.0);
         graphContainer.getChildren().setAll(graph);
     }
+
 
     private void runSimulation() {
         run_button.setDisable(true);
     }
+
+    public void setSelectedCell(ListCell<SimListItem> cell) {
+        if (selectedCell != null) {
+            selectedCell.setStyle("");
+        }
+        selectedCell = cell;
+        if (selectedCell != null) {
+            selectedCell.setStyle("-fx-background-color: lightblue;");
+        }
+    }
+
+    public ListCell<SimListItem> getSelectedCell() {
+        return selectedCell;
+    }
+
+
 }
